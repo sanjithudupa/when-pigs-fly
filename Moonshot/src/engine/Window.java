eeb031bce3c54951b40f99c0e98e5b404f7b4779
Sanjith Udupa <sanjith.udupa@gmail.com>
2020-11-05 22:41:29 -0500

    occlusion culling

--- Moonshot/src/engine/Window.java
+++ Moonshot/src/engine/Window.java
@@ -92,6 +92,9 @@ public class Window {
 
         glEnable(GL_DEPTH_TEST);
 
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+
         // Support for transparencies
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
